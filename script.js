377d8e1a8f2a7ae206fc8e04453a13f19fb3da84
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2023-02-17 23:22:46 +0400

    update

---
+++ script.js
@@ -0,0 +1,63 @@
+function Print() {
+    window.print();
+}
+
+function calculate() {
+    var j = document.getElementById('s').value;
+    var e = document.getElementById('m').value;
+    var s = document.getElementById('c').value;
+    var u = document.getElementById('t').value;
+    var g = e * s;
+    document.getElementById('t').value = g;
+    document.getElementById('s').value = j;
+}
+
+function calculate2() {
+    var j2 = document.getElementById('s2').value;
+    var e2 = document.getElementById('m2').value;
+    var s2 = document.getElementById('c2').value;
+    var u2 = document.getElementById('t2').value;
+    var g2 = e2 * s2;
+    document.getElementById('t2').value = g2;
+    document.getElementById('s2').value = j2;
+}
+
+function calculate3() {
+    var j3 = document.getElementById('s3').value;
+    var e3 = document.getElementById('m3').value;
+    var s3 = document.getElementById('c3').value;
+    var u3 = document.getElementById('t3').value;
+    var g3 = e3 * s3;
+    document.getElementById('t3').value = g3;
+    document.getElementById('s3').value = j3;
+}
+
+function calculate4() {
+    var j4 = document.getElementById('s4').value;
+    var e4 = document.getElementById('m4').value;
+    var s4 = document.getElementById('c4').value;
+    var u4 = document.getElementById('t4').value;
+    var g4 = e4 * s4;
+    document.getElementById('t4').value = g4;
+    document.getElementById('s4').value = j4;
+}
+
+function calculate5() {
+    var j5 = document.getElementById('s5').value;
+    var e5 = document.getElementById('m5').value;
+    var s5 = document.getElementById('c5').value;
+    var u5 = document.getElementById('t5').value;
+    var g5 = e5 * s5;
+    document.getElementById('t5').value = g5;
+    document.getElementById('s5').value = j5;
+}
+
+function calculate6() {
+    var j6 = document.getElementById('s6').value;
+    var e6 = document.getElementById('m6').value;
+    var s6 = document.getElementById('c6').value;
+    var u6 = document.getElementById('t6').value;
+    var g6 = e6 * s6;
+    document.getElementById('t6').value = g6;
+    document.getElementById('s6').value = j6;
+}
